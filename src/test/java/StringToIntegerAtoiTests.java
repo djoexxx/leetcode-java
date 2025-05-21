c39f6f401f41b90d5032b8a3bc22e7966bd1b8e6
Daniel Joe <ukkynubatu@outlook.com>
2025-05-20 23:24:42 -0700

    Added String to Integer (atoi) test solution

---
+++ src/test/java/StringToIntegerAtoiTests.java
@@ -0,0 +1,33 @@
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class StringToIntegerAtoiTests extends StringToIntegerAtoi {
+
+  @Test(dataProvider = "string to int atoi")
+  public void myAtoiTest(String s, int expected) {
+    int converted = myAtoi(s);
+
+    assertThat(converted)
+        .as("Converted: " + converted + "    Expected: " + expected)
+        .isEqualTo(expected);
+  }
+
+  @DataProvider(name = "string to int atoi")
+  public Object[][] stringToAtoi() {
+    return new Object[][] {
+      {"123456", 123456},
+      {"99999999999", Integer.MAX_VALUE},
+      {"-123456", -123456},
+      {"-99999999999999", Integer.MIN_VALUE},
+      {"1234cdfa1324", 1234},
+      {"00000001234", 1234},
+      {"1234.432q5435345354343543", 1234},
+      {"-042", -42},
+      {"+", 0},
+      {" ", 0},
+      {"9223372036854775808", 2147483647}
+    };
+  }
+}
